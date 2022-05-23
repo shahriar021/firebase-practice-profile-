2b4ddeabf86392087a03ee15a77e37d7d7be3c98
shahriar021 <shahriarc19@gmail.com>
2022-05-23 19:40:18 +0600

    firebase

--- app/src/main/java/com/example/firebasecrud2/Adapter.java
+++ app/src/main/java/com/example/firebasecrud2/Adapter.java
@@ -3,7 +3,10 @@ package com.example.firebasecrud2;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,6 +14,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -39,6 +50,62 @@ public class Adapter extends FirebaseRecyclerAdapter<MainModel,Adapter.myviewhol
                 .error(com.firebase.ui.database.R.drawable.common_google_signin_btn_icon_dark_normal)
                 .into(holder.img);
 
+        holder.btnedit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                final DialogPlus dialogPlus= DialogPlus.newDialog(holder.img.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.updte_popup))
+                        .setExpanded(true,800)
+                        .create();
+
+                //dialogPlus.show();
+                View view1 =dialogPlus.getHolderView();
+                EditText name=view1.findViewById(R.id.txtnme);
+                EditText course=view1.findViewById(R.id.txtCourse);
+                EditText email=view1.findViewById(R.id.txtEmail);
+                EditText surl=view1.findViewById(R.id.txtimage);
+
+                Button btnupdate=view1.findViewById(R.id.btnupdate);
+                name.setText(model.getName());
+                course.setText(model.getCourse());
+                email.setText(model.getEmail());
+                surl.setText(model.getSurl());
+
+                dialogPlus.show();
+
+                btnupdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Map<String,Object> map=new HashMap<>();
+                        map.put("name",name.getText().toString());
+                        map.put("course",course.getText().toString());
+                        map.put("email",email.getText().toString());
+                        map.put("imge",surl.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("student")
+                                .child(getRef(holder.getAdapterPosition()).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.name.getContext(), "Data updated successfully", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(holder.name.getContext(), "Error while updating", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                    }
+                });
+
+
+            }
+        });
+
     }
 
     @NonNull
@@ -52,6 +119,7 @@ public class Adapter extends FirebaseRecyclerAdapter<MainModel,Adapter.myviewhol
     class myviewholer extends RecyclerView.ViewHolder{
         CircleImageView img;
         TextView name,course,email;
+        Button btnedit,btndelete;
 
 
         public myviewholer(@NonNull View itemView) {
@@ -61,6 +129,9 @@ public class Adapter extends FirebaseRecyclerAdapter<MainModel,Adapter.myviewhol
             name = (TextView)itemView.findViewById(R.id.txtname);
             course = (TextView)itemView.findViewById(R.id.txtcourse);
             email = (TextView)itemView.findViewById(R.id.txtemail);
+
+            btnedit=(Button) itemView.findViewById(R.id.btnedit);
+            btndelete=(Button) itemView.findViewById(R.id.btndlt);
         }
     }
 }
--- app/src/main/java/com/example/firebasecrud2/MainActivity.java
+++ app/src/main/java/com/example/firebasecrud2/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
@@ -46,5 +49,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         adapter.stopListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search,menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                txtSearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                txtSearch(s);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void txtSearch(String str){
+        FirebaseRecyclerOptions<MainModel> options =
+                new FirebaseRecyclerOptions.Builder<MainModel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("teachers").orderByChild("email").startAt(str).endAt(str+"~"), MainModel.class)
+                        .build();
+
+        adapter =new Adapter(options);
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
     }
 }
